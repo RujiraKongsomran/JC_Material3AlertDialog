4e8a43f15308ff35f84283eef7b14e4d018c2dc5
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-02-09 14:44:36 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/jc_material3alertdialog/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_material3alertdialog/MainActivity.kt
@@ -3,13 +3,20 @@ package com.rujirakongsomran.jc_material3alertdialog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_material3alertdialog.ui.theme.JC_Material3AlertDialogTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +24,68 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JC_Material3AlertDialogTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                CreateDialog()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateDialog() {
+    val openDialog = remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { openDialog.value = true },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Show Dialog")
+        }
+    }
+
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = { openDialog.value = false },
+            icon = {
+                Icon(Icons.Filled.Lock, contentDescription = null)
+            },
+            title = {
+                Text(text = "Dialog with lock icon")
+            },
+            text = {
+                Text(
+                    "This area typically contains the supportive text " +
+                            "which presents the details regarding the Dialog's purpose."
+                )
+
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = { openDialog.value = false },
+
+                    ) {
+                    Text(text = "Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = { openDialog.value = false },
+                ) {
+                    Text(text = "Dismiss")
+                }
+            }
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun CreateDialogPreview() {
     JC_Material3AlertDialogTheme {
-        Greeting("Android")
+        CreateDialog()
     }
 }
